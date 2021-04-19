8ab9b8a612e6f34bc456579c99772f122dff9d97
andrea ilieschi <andrea.ilieschi@gmail.com>
2021-04-19 23:18:27 +0200

    done

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -25,3 +27,55 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+  // Model.create() new recipe
+  // creating a new recipe
+
+const newRecipe =   {
+    title: "Pasta al sughero",
+    level: "Easy Peasy",
+    ingredients: [
+      "spaghetti",
+      "sughero",
+      "salt",
+      "water"
+    ],
+    cuisine: "Italian",
+    dishType: "main_course",
+    image: "https://www.lospicchiodaglio.it/img/ricette/pastasugofilettipesce.jpg",
+    duration: 30,
+    creator: "Chef Andrea",
+    created: "2021-04-19"
+    };
+
+
+// Iteration 2 - 
+// Recipe.create(newRecipe)
+// .then(recipe => console.log('The new recipe is named: ', recipe))
+// .catch(error => console.log('An error happened:', error));
+
+
+// Iteration: 3
+// Recipe.insertMany(data)
+//     .then(recipesFromDB => {
+//       recipesFromDB.forEach(oneRecipe => console.log(`recipe: ${oneRecipe.title}`));
+//     })
+//     .catch(error => console.log(`Error occurred during getting recipes from DB: ${error}`));
+
+
+// Iteration 4 - Update recipe --- findOneAndUpdate deprecated???
+// Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+// .then(duration => console.log('Now the duration is ok'))
+// .catch(error => console.log(`Error occurred during getting recipes from DB: ${error}`));
+
+
+// Iteration 5 - Remove a recipe
+Recipe.deleteOne({ title: "Carrot Cake" })
+.then((carrotC) => {
+  console.log('Bye Bye Carrot Cake')
+  mongoose.connection.close()
+  .then(bye => console.log('Good night DB'))
+})
+.catch(error => console.log('An error happened:', error));
+
